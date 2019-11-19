7c6fd01975ddbd538d790dc0c54227773c9f4a5f
EthanJW21 <EthanJW21@users.noreply.github.com>
2019-11-19 20:26:56 +0000

    Update fizzbuzz.js
    Attempt 5

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,15 @@
 
 // More Information about this assisngment is available in the README.md file
 
-for ( var i = 1; i< 100; i++ ){
-   
-if ( i % 3 == 0 && i % 5 == 0 ){
+for ( var i = 1; i< 100; i++ ) {
+    if ( i % 3 == 0 && i % 5 == 0 ){
         output ("FizzBuzz");
     }
 
-else if ( i % 3 == 0 ){
+    else if ( i % 3 == 0 ){
     output("Fizz");
-} 
-else if ( i % 5 == 0 ){
+ } 
+    else if ( i % 5 == 0 ){
     output("Buzz");
-}
-
+ }
 }
